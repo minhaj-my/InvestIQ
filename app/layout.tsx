f2cdb4455b8ac2389f693f5aee4c51ce4df99e34
Elisha <your-email@example.com>
2026-06-24 23:41:55 +0600

    fix: revert to nodejs runtime for mongoose

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 
-export const runtime = "edge";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
